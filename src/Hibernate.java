fdc0c32a5070eb53a3e75150529cb466564814ba
Tomek <tomasz.ciecwierz@gmail.com>
2018-05-03 15:05:22 +0200

    1n1

--- src/Hibernate.java
+++ src/Hibernate.java
@@ -1,13 +1,9 @@
-import encje.Narzedzia;
+import encje.Adres;
 import encje.Pracownik;
-import encje.Stanowisko;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
-import java.util.HashSet;
-import java.util.Set;
 
 public class Hibernate {
     public static void main(String[] args) {
@@ -15,51 +11,64 @@ public class Hibernate {
         Configuration conf = new Configuration().configure();
         SessionFactory factory = conf.buildSessionFactory();
         Session session = factory.openSession();
-        //List l = session.createQuery("SELECT pracownik.stanowisko FROM Pracownik pracownik").list();
-        //List m = session.createQuery("SELECT pracownik.nazwisko FROM Pracownik pracownik WHERE stanowisko ='Spawacz'").list();
-        //List m = session.createQuery("SELECT pracownik.Nazwisko FROM Pracownik pracownik WHERE Wynagrodzenie > 3000").list();
-        //List p = session.createQuery("SELECT pracownik.Nazwisko FROM Pracownik pracownik WHERE Stanowisko = 'Spawacz'").list();
+//        List l = session.createQuery("SELECT pracownik.stanowisko FROM Pracownik pracownik").list();
+//        List m = session.createQuery("SELECT pracownik.nazwisko FROM Pracownik pracownik WHERE stanowisko ='Spawacz'").list();
+//        List m = session.createQuery("SELECT pracownik.Nazwisko FROM Pracownik pracownik WHERE Wynagrodzenie > 3000").list();
+//        List p = session.createQuery("SELECT pracownik.Nazwisko FROM Pracownik pracownik WHERE Stanowisko = 'Spawacz'").list();
         Transaction transaction = session.beginTransaction();
 
-        /*Stanowisko stanowisko = new Stanowisko();
-        stanowisko.setNazwa("Spawacz");
-        stanowisko.setOpis("prace spawalnicze");
-        Set<Pracownik> zbior = new HashSet<Pracownik>();
-        Pracownik pracownik = new Pracownik();
-        pracownik.setImie("Heniek");
-        pracownik.setNazwisko("Kowalski");
-        pracownik.setStanowisko(stanowisko);
-        zbior.add(pracownik);
-        stanowisko.setPracownik(zbior);
-        session.persist(stanowisko);
-        transaction.commit();*/
+//        Stanowisko stanowisko = new Stanowisko();
+//        stanowisko.setNazwa("Spawacz");
+//        stanowisko.setOpis("prace spawalnicze");
+//        Set<Pracownik> zbior = new HashSet<Pracownik>();
+//        Pracownik pracownik = new Pracownik();
+//        pracownik.setImie("Heniek");
+//        pracownik.setNazwisko("Kowalski");
+//        pracownik.setStanowisko(stanowisko);
+//        zbior.add(pracownik);
+//        stanowisko.setPracownik(zbior);
+//        session.persist(stanowisko);
+//        transaction.commit();
 
+//        Stanowisko stanowisko = new Stanowisko();
+//        stanowisko.setNazwa("Tokarz");
+//        stanowisko.setOpis("prace tokarskie");
+//
+//        Set<Narzedzia> zbiorNarzedzi = new HashSet<Narzedzia>();
+//        Narzedzia narzedzia = new Narzedzia();
+//        narzedzia.setTyp("Nóż tokarski");
+//        narzedzia.setSerialNumber("5354-456");
+//        zbiorNarzedzi.add(narzedzia);
+//
+//        Set<Pracownik> zbiorPracownikow = new HashSet<Pracownik>();
+//        Pracownik pracownik = new Pracownik();
+//        pracownik.setImie("Waldek");
+//        pracownik.setNazwisko("Okrągły");
+//        pracownik.setNarzedzia(zbiorNarzedzi);
+//
+//        zbiorPracownikow.add(pracownik);
+//        stanowisko.setPracownik(zbiorPracownikow);
+//        session.persist(stanowisko);
+//        transaction.commit();
 
-        Stanowisko stanowisko = new Stanowisko();
-        stanowisko.setNazwa("Tokarz");
-        stanowisko.setOpis("prace tokarskie");
+//        pracownik = session.get(Pracownik.class, 10);
+//        System.out.println(pracownik.getImie() + " " + pracownik.getNazwisko());
+//        stanowisko = session.get(Stanowisko.class, 6);
+//        System.out.println(stanowisko.getNazwa());
+//        System.out.println(stanowisko.getPracownik().iterator().next().getImie() + " " + stanowisko.getPracownik().iterator().next().getNazwisko() + " " + stanowisko.getNazwa());
 
-        Set<Narzedzia> zbiorNarzedzi = new HashSet<Narzedzia>();
-        Narzedzia narzedzia = new Narzedzia();
-        narzedzia.setTyp("Nóż tokarski");
-        narzedzia.setSerialNumber("5354-456");
-        zbiorNarzedzi.add(narzedzia);
+        Pracownik pracownik2 = new Pracownik();
+        pracownik2.setImie("Zdzisław");
+        pracownik2.setNazwisko("Chrobry");
 
-        Set<Pracownik> zbiorPracownikow = new HashSet<Pracownik>();
-        Pracownik pracownik = new Pracownik();
-        pracownik.setImie("Waldek");
-        pracownik.setNazwisko("Okrągły");
-        pracownik.setNarzedzia(zbiorNarzedzi);
+        Adres adres = new Adres();
+        adres.setMiasto("Warszawa");
+        adres.setUlica("Marszałkowska");
+        adres.setNumerDomu(15);
 
-        zbiorPracownikow.add(pracownik);
-        stanowisko.setPracownik(zbiorPracownikow);
-        session.persist(stanowisko);
+        pracownik2.setAdres(adres);
+        adres.setPracownik(pracownik2);
+        session.persist(pracownik2);
         transaction.commit();
-
-        pracownik = session.get(Pracownik.class, 10);
-        //System.out.println(pracownik.getImie() + " " + pracownik.getNazwisko());
-        stanowisko = session.get(Stanowisko.class, 6);
-        //System.out.println(stanowisko.getNazwa());
-        System.out.println(stanowisko.getPracownik().iterator().next().getImie() + " " + stanowisko.getPracownik().iterator().next().getNazwisko() + " " + stanowisko.getNazwa());
     }
 }
